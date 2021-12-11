b82692aa623392b15078ef1d33d0795c2e2e7209
Fhurrur <9593889+Fhurrur@users.noreply.github.com>
2021-12-11 14:26:10 +0100

    day11

---
+++ 2021/day11part2.js
@@ -0,0 +1,56 @@
+import {DAY11} from "./data.js";
+
+const board = DAY11.trim().split('\n')
+    .map(line => [...line].map(v => ({flashed: false, value: parseInt(v)})));
+
+let totalFlashes = 0;
+
+const SIZE = 10;
+for (let step = 0; step < Number.MAX_SAFE_INTEGER; step++) {
+    let allFlashed = true;
+    for (let i = 0; i < SIZE; i++) {
+        for (let j = 0; j < SIZE; j++) {
+            allFlashed = allFlashed && board[i][j].value === 0;
+            board[i][j].value++;
+        }
+    }
+
+    if (allFlashed) {
+        console.log("All flashed at " + step);
+        break;
+    }
+
+    let changed = true;
+    while (changed) {
+        changed = false;
+        for (let i = 0; i < SIZE; i++) {
+            for (let j = 0; j < SIZE; j++) {
+                let element = board[i][j];
+                if (element.value >= 10 && !element.flashed) {
+                    changed = true;
+                    element.flashed = true;
+                    inc(board, i, j);
+                }
+            }
+        }
+    }
+
+    for (let i = 0; i < SIZE; i++) {
+        for (let j = 0; j < SIZE; j++) {
+            let element = board[i][j];
+            if (element.value >= 10) {
+                totalFlashes++;
+                element.value = 0;
+            }
+            element.flashed = false;
+        }
+    }
+}
+
+function inc(board, i, j) {
+    for (let k = Math.max(0, i - 1); k <= Math.min(SIZE - 1, i + 1); k++) {
+        for (let l = Math.max(0, j - 1); l <= Math.min(SIZE - 1, j + 1); l++) {
+            board[k][l].value++;
+        }
+    }
+}
