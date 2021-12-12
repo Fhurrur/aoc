a6697cd50e8432a8941d3b0b6171d0ef0baff16c
Fhurrur <9593889+Fhurrur@users.noreply.github.com>
2021-12-12 17:12:33 +0100

    day12

---
+++ 2021/day12part2.js
@@ -0,0 +1,38 @@
+import {DAY12} from "./data.js";
+
+const paths = DAY12.trim().split('\n')
+    .map(line => {
+        let nodes = line.split("-");
+        return {start: nodes[0], end: nodes[1]}
+    });
+const nodes = {};
+paths.forEach(path => {
+    if (!nodes[path.start]) {
+        nodes[path.start] = {nodes: [], big: path.start.toLowerCase() !== path.start};
+    }
+    nodes[path.start].nodes.push(path.end);
+    if (!nodes[path.end]) {
+        nodes[path.end] = {nodes: [], big: path.end.toLowerCase() !== path.end};
+    }
+    nodes[path.end].nodes.push(path.start);
+});
+
+function visit(node, path, visitedSmallCaveAlready) {
+    let localPath = new Array(...path);
+    localPath.push(node);
+    if (node === 'end') {
+        return [localPath];
+    }
+    return nodes[node].nodes
+        .filter(node => node !== 'start')
+        .flatMap(node => {
+            let smallAndVisited = !nodes[node].big && path.includes(node);
+            if (smallAndVisited && visitedSmallCaveAlready) {
+                return [];
+            }
+            return [...visit(node, localPath, smallAndVisited || visitedSmallCaveAlready)];
+        })
+}
+
+console.log(visit('start', [], false).length)
+
